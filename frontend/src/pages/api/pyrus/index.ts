69939d10e5dce86a61364b6cc0678dc6ebc80666
tp <61617660+ittp@users.noreply.github.com>
2023-02-08 15:49:11 +0300

    Create index.ts

---
+++ frontend/src/pages/api/pyrus/index.ts
@@ -0,0 +1,77 @@
+let demoTask = {
+  "task": {
+    "id": 11611,
+    "text": "Help with printer",
+    "create_date": "2017-08-17T15:32:11Z",
+    "last_modified_date": "2017-08-17T15:32:11Z",
+    "author": {
+      "id": 1731,
+      "first_name": "Bob",
+      "last_name": "Smith",
+      "email": "Bob.Smith@gmail.com",
+      "type": "user"
+    },
+    "responsible": {
+      "id": 1733,
+      "first_name": "John",
+      "last_name": "Snow",
+      "email": "John.Snow@gmail.com",
+      "type": "user"
+    },
+    "participants": [
+      {
+        "id": 1733,
+        "first_name": "John",
+        "last_name": "Snow",
+        "email": "John.Snow@gmail.com",
+        "type": "user"
+      },
+      {
+        "id": 1731,
+        "first_name": "Bob",
+        "last_name": "Smith",
+        "email": "Bob.Smith@gmail.com",
+        "type": "user"
+      }
+    ],
+    "comments": [
+      {
+        "id": 13767,
+        "create_date": "2017-08-17T15:32:11Z",
+        "text": "Help with printer",
+        "author": {
+          "id": 1731,
+          "first_name": "Bob",
+          "last_name": "Smith",
+          "email": "Bob.Smith@gmail.com",
+          "type": "user"
+        },
+        "reassigned_to": {
+          "id": 1730,
+          "first_name": "John",
+          "last_name": "Snow",
+          "email": "John.Snow@gmail.com",
+          "type": "user"
+        },
+        "participants_added": [
+          {
+            "id": 1733,
+            "first_name": "John",
+            "last_name": "Snow",
+            "email": "John.Snow@gmail.com",
+            "type": "user"
+          },
+          {
+            "id": 1731,
+            "first_name": "Bob",
+            "last_name": "Smith",
+            "email": "Bob.Smith@gmail.com",
+            "type": "user"
+          }
+        ],
+        "reply_note_id": "13768",
+      }
+    ]
+  }
+}
+
